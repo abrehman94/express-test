02b68f1b3cae723586a300daeb74404a64034c9a
abrehman94 <abrehman1994@gmail.com>
2017-09-14 01:47:17 +0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,28 @@
+var express = require("express");
+var hbs= require("hbs");
+
+hbs.registerPartials(__dirname +"/views/partials");
+hbs.registerHelper("currentYear", ()=>{
+    var time= new Date().getFullYear();
+    return time;
+});
+var app= express();
+
+app.set("view enginge", "hbs");
+app.use (function(req, res, next){
+    console.log("lololol");
+    next();
+    console.log(next);
+});
+app.use(express.static( __dirname+"/public"));
+
+
+
+app.get("/", function(req, res){
+    res.render("home.hbs", {name:"Abdul rehman", cDate: new Date().getFullYear()});
+});
+
+
+
+
+app.listen(2000);
